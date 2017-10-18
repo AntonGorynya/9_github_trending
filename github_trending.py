576f1e391303b659e4e74066afcef9f00e27b1cb
Anton <antongorynya1@gmail.com>
2017-10-18 15:33:44 +0300

    Update github_trending.py

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,32 @@
+import requests
+from datetime import date, timedelta
 
 
 def get_trending_repositories(top_size):
-    pass
+    start_date = date.today() - timedelta(weeks=1)
+    url = "https://api.github.com/search/repositories"
+    payload = {'q': "created:>{}".format(start_date), "sort": "stars"}
+    top_repo = requests.get(url, params=payload).json()['items'][:top_size]
+    return top_repo
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    amount = 0
+    url = "https://api.github.com/search/issues"
+    payload = {'q': "user:{}".format(repo_owner['login']), 'repo': repo_name}
+    issues = requests.get(url, params=payload)
+    for item in issues.json()['items']:
+        if item['state'] == 'open':
+            amount += 1
+    return amount
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 5
+    for repo in get_trending_repositories(top_size):
+        repo_owner = repo['owner']
+        repo_name = repo['name']
+        print('repository: {}. Created by {}'
+              .format(repo_name, repo_owner['login']))
+        print('open issues amount: {}'
+              .format(get_open_issues_amount(repo_owner, repo_name)))
